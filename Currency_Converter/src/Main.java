c7a13c064666018b7d4573738ca982c8e5f330ba
Yiğit Gökkaya <90151779+yigitgokkaya@users.noreply.github.com>
2021-09-19 18:39:23 +0300

    Delete Currency_Converter directory

--- Currency_Converter/src/Main.java
+++
@@ -1,9 +0,0 @@
-import org.json.JSONException;
-import java.io.IOException;
-
-
-public class Main {
-    public static void main(String[] args) throws JSONException, IOException {
-        Menu.Menu();
-    }
-}
